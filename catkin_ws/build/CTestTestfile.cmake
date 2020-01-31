cf5e424ed61a060171525205f28e150421eef726
khan496 <khan4@clemson.edu>
2020-01-30 23:50:59 -0500

    Assignment 2 files

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/anas/AuE893Spring20_MohammadAnasImamKhan/catkin_ws/src
+# Build directory: /home/anas/AuE893Spring20_MohammadAnasImamKhan/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(assignment2_ws)
+subdirs(turtlesim_cleaner)
