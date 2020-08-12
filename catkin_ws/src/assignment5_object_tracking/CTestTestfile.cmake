77fede34c963f9fda8662b46f8b4d9e3e6a7b5be
Mohammad Anas <khananas26@gmail.com>
2020-08-12 08:11:48 -0400

    Add files via upload

---
+++ catkin_ws/src/assignment5_object_tracking/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anas/AuE893Spring20_MohammadAnasImamKhan/catkin_ws/src/assignment5_object_tracking
+# Build directory: /home/anas/AuE893Spring20_MohammadAnasImamKhan/catkin_ws/src/assignment5_object_tracking
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/src/assignment5_object_tracking/src/check_cv_version.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+
+#print(cv2._version_)
+print(cv2.__version__)
